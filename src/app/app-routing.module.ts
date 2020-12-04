96bdf23c205f5e5553ea253f2c5dafe302c64987
hilal-coskun <hilal.coskun20@gmail.com>
2020-12-05 02:00:48 +0300

    template

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { DefaultComponent } from './layouts/default/default.component';
+import { FullwidthComponent } from './layouts/fullwidth/fullwidth.component';
+import { HomeComponent } from './modules/home/home.component';
+import { LoginComponent } from './modules/login/login.component';
+import { PostsComponent } from './modules/posts/posts.component';
+
+const routes: Routes = [{
+  path: '',
+  component: DefaultComponent,
+  children:[{
+    path:'',
+    component:HomeComponent
+  },
+  {
+    path: 'posts',
+    component: PostsComponent
+  }]
+},{
+  path: '',
+  component: FullwidthComponent,
+  children:[
+    {
+    path: 'login',
+    component: LoginComponent
+  }]
+}];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
